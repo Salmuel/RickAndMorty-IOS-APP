2cbf195b1b13fd0ef441cce2786a62a9f43f5b5b
Salmuel <ssalama@iu.edu>
2023-02-13 20:02:18 -0500

    changes/adds

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,14 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Samy Salama on 2/13/23.
+//
+
+import Foundation
+
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Samy Salama on 2/13/23.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable{
+    let name: String
+    let url: String
+}
